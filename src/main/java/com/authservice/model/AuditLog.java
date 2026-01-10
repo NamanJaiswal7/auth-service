3991d94db523df8e16cd20941eeb62dedde6cab1
namanjaiswal7 <naman.jaiswal1@gmail.com>
2026-01-10 15:20:10 +0200

    implementation of audit logging system

---
+++ src/main/java/com/authservice/model/AuditLog.java
@@ -0,0 +1,31 @@
+package com.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "audit_logs")
+public class AuditLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userEmail;
+    private String action;
+    private String details;
+    private String ipAddress;
+
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+}
